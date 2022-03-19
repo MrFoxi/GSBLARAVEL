7ab124b3f85584bdab4758f921934cfce394b22e
guillaume <guillaume.kerherve@orange.fr>
2022-03-19 15:46:21 +0100

    resolution probleme compilation mix

--- resources/views/praticien.blade.php
+++ resources/views/praticien.blade.php
@@ -11,10 +11,31 @@
                     <label for="label">recherche</label>
                     <input list="browsers" name="browser" id="browser">
                         <datalist id="browsers">
-                            @foreach($praticien as $praticiens)
-                             <option value="{{$praticiens['PRA_NOM']}} {{$praticiens['PRA_PRENOM']}}">
-                            @endforeach
                         </datalist>
+                        <table class="table table-dark table-striped">
+                            <tr>
+                                <th>Num</th>
+                                <th>Nom</th>
+                                <th>Prenom</th>
+                                <th>Adresse</th>
+                                <th>Code postal</th>
+                                <th>Ville</th>
+                                <th>Notoriété</th>
+                                <th>Type de praticien</th>
+                            </tr>
+                        @foreach($praticien as $praticiens)
+                            <tr>
+                                <td>{{$praticiens['PRA_NUM']}}</td>
+                                <td>{{$praticiens['PRA_NOM']}}</td>
+                                <td>{{$praticiens['PRA_PRENOM']}}</td>
+                                <td>{{$praticiens['PRA_ADRESSE']}}</td>
+                                <td>{{$praticiens['PRA_CP']}}</td>
+                                <td>{{$praticiens['PRA_VILLE']}}</td>
+                                <td>{{$praticiens['PRA_COEFNOTORIETE']}}</td>
+                                <td>{{$praticiens['TYP_CODE']}}</td>
+                            </tr>
+                        @endforeach
+                    </table>
                 </form>
             </div>
         </div>
